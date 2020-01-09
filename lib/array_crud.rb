47e722e7324ee5745efbf21a7eab02e8491ace82
Eduardo mendoza <garibo.eduardo90@gmail.com>
2020-01-09 21:07:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,15 +7,15 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  arr << element
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  arr.unshift(element)
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  arr.pop()
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
