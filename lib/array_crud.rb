b5da4c2a55254ef31fc813019b06daaa56ae5966
Eduardo mendoza <garibo.eduardo90@gmail.com>
2020-01-09 21:07:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,19 +3,19 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  arr = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  arr << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  arr.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  arr.pop()
 end
 
 def remove_element_from_start_of_array(array)
